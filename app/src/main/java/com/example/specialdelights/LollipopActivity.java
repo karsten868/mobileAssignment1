3ea3fc8527743d7b99c92dd3b2811f984696433b
KARSTEN LANGE <karstenlange1999@gmail.com>
2021-10-18 06:37:57 -0400

    second commit

---
+++ app/src/main/java/com/example/specialdelights/LollipopActivity.java
@@ -0,0 +1,14 @@
+package com.example.specialdelights;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LollipopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lollipop);
+    }
+}
